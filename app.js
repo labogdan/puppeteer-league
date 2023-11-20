daf1c84a03ef73ee591177f0c15b1edf0ffc4216
Andrew Bogdan <abogdan@Andrews-MacBook-Air.local>
2023-11-20 14:27:41 -0500

    change for frontend

--- app.js
+++ app.js
@@ -54,10 +54,8 @@ app.get('/check-file-exists', (req, res) => {
 
 app.use(express.static(path.join(__dirname, './frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
-});
-
+app.use(express.static(__dirname + '/frontend/build'));
+app.use(express.static('public'));
 
 
 server.listen(port, () => {
